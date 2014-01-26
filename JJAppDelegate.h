fbfa2501f5546fb209cbb7f5e3be76bef0a95099
Jonathan Jayet <jonathan@jayet.eu>
2014-01-26 23:32:17 +0100

    Initial commit
    The first of all !

---
+++ JJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JJAppDelegate.h
+//  HideAndBeak
+//
+//  Created by Jonathan Jayet on 26/01/2014.
+//  Copyright (c) 2014 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
